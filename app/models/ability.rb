9928fe469172ce424a5864db144fbed6257a0cc5
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 16:31:09 +0700

    ability_for_user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    # ability for all user (even not logged in)
     can :read, Post
+
+    if user
+      # ability for logged in usser
+      can :manage, Post, user_id: user.id
+    end
   end
 end
