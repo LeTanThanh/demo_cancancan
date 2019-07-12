f763069365bc0fbde4dbf1a1ddd9f2d0b83d8dd2
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 16:36:19 +0700

    add_is_admin_to_users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+User.create name: "TanThanhle", email: "le.tan.thanh@sun-asterisk.com",
+  password: "Aa@123456", password_confirmation: "Aa@123456", is_admin: true
+
 5.times do
   user = User.create name: FFaker::Name.name, email: FFaker::Internet.email,
     password: "Aa@123456", password_confirmation: "Aa@123456"
