bb9f84086a3ec3229cdc952c08c7b67ef3682301
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 14:34:43 +0700

    add_header_and_flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def alert_class type
+    case type
+    when "alert"
+      "alert-danger"
+    when "notice"
+      "alert-success"
+    else
+      "alert-#{type}"
+    end
+  end
 end
