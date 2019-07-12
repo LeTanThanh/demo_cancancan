376059faf82631d94b1adaf1e4a88cab9a13873a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 14:03:46 +0700

    add_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  user = User.create name: FFaker::Name.name, email: FFaker::Internet.email,
+    password: "Aa@123456", password_confirmation: "Aa@123456"
+
+  5.times do
+    user.posts.create title: FFaker::Lorem.sentence, body: FFaker::Lorem.paragraphs.join()
+  end
+end
