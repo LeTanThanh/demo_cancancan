3057f05fa3cf7db22da76002b83b1748488f1162
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 16:40:02 +0700

    ability_for_admin_user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,10 @@ class Ability
     if user
       # ability for logged in usser
       can :manage, Post, user_id: user.id
+
+      if user.is_admin?
+        can :manage, :all
+      end
     end
   end
 end
